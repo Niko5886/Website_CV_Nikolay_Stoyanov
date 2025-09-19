47e04cc59e2b190bfc5d22a852337c8754130b99
Niko5886 <lobido1988@gmail.com>
2025-09-19 14:59:08 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -624,9 +624,10 @@ const App = () => {
       <div className="fixed inset-0 overflow-hidden pointer-events-none">
         <div className="absolute -top-40 -right-40 w-80 h-80 bg-cyan-500/30 rounded-full blur-3xl animate-pulse"></div>
         <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-purple-500/30 rounded-full blur-3xl animate-pulse delay-1000"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-pink-500/20 rounded-full blur-3xl animate-pulse delay-2000"></div>
       </div>
 
-      {/* Main Content */}
+      {/* Content */}
       <main className="relative z-10">
         {renderContent()}
       </main>
